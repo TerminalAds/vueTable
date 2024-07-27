deef81ae46051dad033c4b292fa376d7733e5d53
vahid <5300terminalads@gmail.com>
2024-07-27 10:11:12 +0330

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # vueTable
+
 a custom vue data table module
+
+## Table Of Content.
+
+- [Dependency](#Dependency).
+- [Usage](#Usage).
+- [Update](#Update).
+
+-----
+
+### Dependency.
+
+1. Vue.
+2. Vuetify.
+
+### Usage.
+
+First clone this repository in your project or add as submodule.
+
+```bash
+git clone git@github.com:TerminalAds/vueTable.git
+```
+
+or
+
+```bash
+git submodule add git@github.com:TerminalAds/vueTable.git path/where/you/want
+
+#for example.
+git submodule add git@github.com:TerminalAds/vueTable.git src/core/plugins/vueTable
+
+#if module doesn't clone successfully try these 2 steps.
+git submodule init
+git submodule update
+```
+
+### Update.
+
+```git
+git submodule update --remote
+```
---
+++ src/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :itemsPerPage="itemsPerPage"
+    :loading="loading"
+    hide-default-footer
+    show-select>
+    <template v-for="(slot, i) in $slots" v-slot:item.[slot]="{ item }">
+
+    </template>
+
+    <!--    <template v-slot:item.clicks="{ item }">-->
+    <!--      <span>{{ item.clicks || 0 }}</span>-->
+    <!--    </template>-->
+
+    <!--    <template v-slot:item.prices="{ item }">-->
+    <!--      {{-->
+    <!--        item.prices && item.prices.length > 0-->
+    <!--          ? item.prices[0].expire_at_days + ' روز تا انقضا'-->
+    <!--          : ' منقضی شده'-->
+    <!--      }}-->
+    <!--    </template>-->
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: "index.vue",
+
+  props: {
+    items: Array,
+    itemsPerPage: Number,
+    loading: Boolean,
+    headers: Array,
+  },
+
+  data: () => ({
+    itemsPerPage: 10,
+    options: {},
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
